a8cdb8524b1e8dd227c0e065259439df521dc54e
BuildTools <williamcaetano14@gmail.com>
2021-07-31 19:02:49 -0300

    :lipstick: Added default theme

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-      
+      <p>teste</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-size: 16px;
+  }
+`
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={{}}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
