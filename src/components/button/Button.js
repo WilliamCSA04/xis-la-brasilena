9bd5813f0b6ef1ee3f2229392dcde3dc4ac2e6fc
BuildTools <williamcaetano14@gmail.com>
2021-08-19 23:24:05 -0300

    :recycle: Button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,23 +1,5 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { propTypesChildren } from '../../helpers';
-import { typeSafeguard } from './Button.helpers';
+import styled from 'styled-components';
 
-export default function Button({ children, type, onClick, ...props }) {
-  const validType = typeSafeguard(type);
-  return (
-    <button type={validType} onClick={onClick} {...props}>
-      {children}
-    </button>
-  );
-}
+const Button = styled.button``;
 
-Button.propTypes = {
-  type: PropTypes.oneOf(['button', 'submit', 'reset']),
-  onClick: PropTypes.func.isRequired,
-  children: propTypesChildren.isRequired,
-};
-
-Button.defaultProps = {
-  type: 'submit',
-};
+export default Button;
