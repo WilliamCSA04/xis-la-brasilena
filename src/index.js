bbab102ce7f89f420eb0f19419edbcf7cab2c09c
BuildTools <williamcaetano14@gmail.com>
2021-08-22 14:26:52 -0300

    :truck: Changed Theme from home folder to src folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Theme from './pages/theme';
+import Theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
