183b998cbca5b51903cc53d3ff1cd6f9ef077ca6
BuildTools <williamcaetano14@gmail.com>
2021-09-03 21:40:57 -0300

    :sparkles: Added missing order text

--- src/pages/home/texts.js
+++ src/pages/home/texts.js
@@ -4,7 +4,7 @@ export default {
       title: 'Xis la brasileña',
       postTitle: ' de',
       highlightInfo: '15 cm de diametro',
-      info: 'Horários de Funcionamento: 5pm ás 11pm',
+      info: 'Horários de Funcionamento: 17h ás 23h sábados e domingos',
     },
     menu: {
       title: 'Cardapio',
@@ -28,11 +28,56 @@ export default {
       description:
         'Pan 15cm de diametro, 150g de hamburguesa de Pollo Mayonesa artesanal de la casa Huevo tipo Omelet, queso Choclito americano, alverjita Tomate, lechuga Chatchup y mostaza.',
       postTitle: '15cm',
-      info: 's/15',
+      info: 's/16',
     },
     footer: {
       shareSection: {
         title: 'Compartilhe:',
+      },
+      order: {
+        request: 'Pedidos',
+        cards: 'Todos so cartões aceitos',
+      },
+    },
+  },
+  es: {
+    heroBanner: {
+      title: 'Xis la brasileña',
+      postTitle: ' de',
+      highlightInfo: '15 cm de diámetro',
+      info: 'Horario de atención: 5pm ás 11pm sábados y domingos',
+    },
+    menu: {
+      title: 'Menu',
+    },
+    cheeseburgerMeat: {
+      title: 'Xis Hamburguesa de Carne',
+      description:
+        'Pan 15cm de diametro, 150g de hamburguesa de Carne Mayonesa artesanal de la casa Huevo tipo Omelet, queso Choclito americano, alverjita Tomate, lechuga Chatchup y mostaza.',
+      postTitle: '15cm',
+      info: 's/15',
+    },
+    cheeseburgerPork: {
+      title: 'Xis Hamburguesa de Cerdo',
+      description:
+        'Pan 15cm de diametro, 150g de hamburguesa de Cerdo Mayonesa artesanal de la casa Huevo tipo Omelet, queso Choclito americano, alverjita Tomate, lechuga Chatchup y mostaza.',
+      postTitle: '15cm',
+      info: 's/15',
+    },
+    cheeseburgerChicken: {
+      title: 'Xis Hamburguesa de Pollo',
+      description:
+        'Pan 15cm de diametro, 150g de hamburguesa de Pollo Mayonesa artesanal de la casa Huevo tipo Omelet, queso Choclito americano, alverjita Tomate, lechuga Chatchup y mostaza.',
+      postTitle: '15cm',
+      info: 's/16',
+    },
+    footer: {
+      shareSection: {
+        title: 'Comparte:',
+      },
+      order: {
+        request: 'Pedidos',
+        cards: 'Aceptamos todas las tarjetas',
       },
     },
   },
